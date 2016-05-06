7f7ab4520c620f6d4000778af80292cfc29451c5
BobbieO <bobbie2504@msn.com>
2016-05-06 13:21:44 -0500

    Added more clarifying info boxes

--- update_exercises.sql
+++ update_exercises.sql
@@ -2,22 +2,33 @@ USE codeup_test_db;
 
 SELECT 'All albums in the table' AS 'Info';
 
-SELECT * FROM albums;
+    SELECT * FROM albums;
+
+SELECT 'Increase sales' AS 'New Info';
+
+    UPDATE albums SET sales = (sales * 10);
+
+    SELECT * FROM albums;
 
 SELECT 'All albums before 1980' AS 'Info';
 
-SELECT * FROM albums WHERE release_date < 1980;
+    SELECT * FROM albums WHERE release_date < 1980;
+
+SELECT 'Go back in time' AS 'New Info';
+
+    UPDATE albums SET release_date = 1901 WHERE release_date < 1980;
+
+    SELECT * FROM albums WHERE release_date < 1980;
 
 SELECT 'All Michael, all the time' AS 'Info';
 
-SELECT * FROM albums WHERE artist = 'Michael Jackson';
+    SELECT * FROM albums WHERE artist = 'Michael Jackson';
+
+SELECT 'Michael had a name change' AS 'New Info';
+
+    UPDATE albums SET artist = 'Peter Jackson' Where artist = 'Michael Jackson';
+
+    SELECT * From albums WHERE artist = 'Michael Jackson';
 
 
 
-
-
-
-
-
-
--- UPDATE albums SET labelname = 'value' WHERE label = 'value';
