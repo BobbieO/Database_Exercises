89f44101603e33e69377d808f3ca30b6c77e3e16
BobbieO <bobbie2504@msn.com>
2016-05-06 10:08:26 -0500

    Added more items info to table

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,2 +1,11 @@
 USE codeup_test_db;
 
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES  ('Michael Jackson', 'Thriller', 1982, 48.1, 'Pop'),
+        ('AC/DC', 'Back in Black', 1980, 25.9, 'Hard Rock'),
+        ('Pink Floyd', 'The Dark Side of the Moon', 1973, 22.7, 'Progressive Rock'),
+        ('Whitney Houston', 'The Bodyguard', 1992, 27.4, 'Soundtrack'),
+        ('Meat Loaf', 'Bat Out of Hell', 1977, 20.6, 'Hard Rock'),
+        ('Eagles', 'Their Greatest Hits', 1976, 32.2, 'Soft Rock');
+
+
