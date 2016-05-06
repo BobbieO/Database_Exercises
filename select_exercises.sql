24b950d61874d907bb49a2767d45250c3a034545
BobbieO <bobbie2504@msn.com>
2016-05-06 11:13:56 -0500

    Initial commit of exercise

---
+++ select_exercises.sql
@@ -0,0 +1,26 @@
+USE codeup_test_db;
+
+SELECT 'This is all Pink Floyd albums' AS 'Info';
+
+SELECT * FROM albums WHERE artist = 'Pink Floyd';
+
+SELECT 'This is the year Thriller was released' AS 'Info';
+
+SELECT release_date FROM albums WHERE name = 'Thriller';
+
+SELECT 'This is the genre for Bat Out of Hell' AS 'Info';
+
+SELECT genre FROM albums WHERE name = 'Bat Out of Hell';
+
+SELECT 'These albums were released in the 90s' AS 'Info';
+
+SELECT name FROM albums WHERE release_date > 1989;
+
+SELECT 'These albums had less than 25 million sales' AS 'Info';
+
+SELECT name FROM albums WHERE sales <= 25;
+
+SELECT 'These are the rock albums' AS 'Info';
+
+SELECT name FROM albums WHERE genre = 'Rock';
+
