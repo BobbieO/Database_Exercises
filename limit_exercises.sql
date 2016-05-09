7c171d6d78494ac5b099ad29cac118f540a3f095
BobbieO <bobbie2504@msn.com>
2016-05-09 11:41:36 -0500

    Tenth batch

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -10,3 +10,10 @@ WHERE (birth_date LIKE '%-12-25'
     AND hire_date LIKE '199%-%-%')
 ORDER BY birth_date ASC, hire_date DESC
 LIMIT 5;
+
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date LIKE '%-12-25'
+    AND hire_date LIKE '199%-%-%')
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 5 OFFSET 45;
