9a5e4cabd09e5314a72c72f4f3b056d0cf643506
BobbieO <bobbie2504@msn.com>
2016-05-09 13:55:28 -0500

    Group by unique q names

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -18,7 +18,9 @@ WHERE (birth_date LIKE '%-12-25'
     AND hire_date LIKE '199%-%-%')
 ORDER BY birth_date ASC, hire_date DESC;
 
-SELECT first_name, last_name
+SELECT COUNT(DISTINCT last_name, first_name), last_name, first_name
 FROM employees
 WHERE last_name LIKE '%q%'
-    AND last_name NOT LIKE '%qu%';
+    AND last_name NOT LIKE '%qu%'
+GROUP BY last_name
+ORDER BY COUNT(DISTINCT last_name, first_name);
