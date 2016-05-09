29c6f89e77920dcf8cdb97cbb65b4fa77846f051
BobbieO <bobbie2504@msn.com>
2016-05-09 11:39:40 -0500

    Xmas employees limited to 5

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -3,3 +3,10 @@ SELECT DISTINCT last_name
 FROM employees
 GROUP BY last_name DESC
 LIMIT 10;
+
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date LIKE '%-12-25'
+    AND hire_date LIKE '199%-%-%')
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 5;
