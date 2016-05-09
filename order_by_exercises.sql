768830b6ff4460689a6e960a35de601ae7f19df7
BobbieO <bobbie2504@msn.com>
2016-05-09 10:35:13 -0500

    Initial commit after rename, order Irena's group by first name

---
+++ order_by_exercises.sql
@@ -0,0 +1,28 @@
+SELECT first_name, last_name
+FROM employees
+WHERE gender = 'M'
+    AND (first_name = 'Irena'
+    OR first_name = 'Vidya'
+    OR first_name = 'Maya')
+ORDER BY first_name;
+
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+    OR last_name LIKE '%E'; 
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+    AND last_name LIKE '%E'; 
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE birth_date LIKE '%-12-25'
+    AND hire_date LIKE '199%-%-%';
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+    AND last_name NOT LIKE '%qu%';
