9c24837104d8d3e9aae3fcc9a622961a05a0bb53
BobbieO <bobbie2504@msn.com>
2016-05-09 13:29:35 -0500

    Gender count

---
+++ functions_exercises.sql
@@ -0,0 +1,30 @@
+SELECT CONCAT( COUNT(*), ' ', gender)
+FROM employees
+WHERE (first_name = 'Irena'
+    OR first_name = 'Vidya'
+    OR first_name = 'Maya') 
+GROUP BY gender;
+
+
+SELECT last_name, emp_no
+FROM employees
+WHERE last_name LIKE 'E%'
+    OR last_name LIKE '%E'
+ORDER BY emp_no DESC; 
+
+SELECT last_name, emp_no
+FROM employees
+WHERE last_name LIKE 'E%'
+    AND last_name LIKE '%E'
+ORDER BY emp_no DESC;
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE (birth_date LIKE '%-12-25'
+    AND hire_date LIKE '199%-%-%')
+ORDER BY birth_date ASC, hire_date DESC;
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+    AND last_name NOT LIKE '%qu%';
